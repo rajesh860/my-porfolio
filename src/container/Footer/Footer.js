ce5ef7b92e6bee18c3debc0475349b357eb155ef
Rajesh <65409310+rajesh860@users.noreply.github.com>
2024-12-20 12:40:03 +0530

    Update Footer.js

--- src/container/Footer/Footer.js
+++ src/container/Footer/Footer.js
@@ -12,7 +12,7 @@ const Footer = () => {
       <div className="app__footer-cards">
         <div className="app__footer-card">
           <img src={images.email} alt="Email..." />
-          <a href="mailto:rr396312@gmail.com" className='p-text'>rr396312@gmail</a>
+          <a href="mailto:rr396312@gmail.com" className='p-text'>rajesh.rajput.dev@gmail.com</a>
         </div>
         <div className="app__footer-card">
           <img src={images.mobile} alt="Mobile..." />
